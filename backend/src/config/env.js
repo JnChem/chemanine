604a162fd8c08618e0cc7265ed7766bb1b935e62
jnchem <jacinta.n.chemane@gmail.com>
2026-02-26 02:57:10 +0200

    deployment config added

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const ENV = 
 {
     NODE_ENV:process.env.NODE_ENV,
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
-}
+};
